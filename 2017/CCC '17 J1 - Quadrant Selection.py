751e2171600c1efc79fdf446de05d069a22958dd
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-01-06 12:33:30 -0500

    .

---
+++ 2017/CCC '17 J1 - Quadrant Selection.py
@@ -0,0 +1,14 @@
+#Link: https://dmoj.ca/problem/ccc17j1
+#Made by Vishwas Puri
+
+x = int(input())
+y = int(input())
+
+if x>=0 and y>=0:
+    print("1")
+elif x<=0 and y>=0:
+    print("2")
+elif x<=0 and y<=0:
+    print("3")
+else:
+    print("4")
