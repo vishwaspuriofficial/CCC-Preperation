10ee536e988cc612e34a1781f2bb29c191eaaea0
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-01-31 19:27:34 -0500

    .

---
+++ 2019/CCC '19 J2 - Time to Decompress.py
@@ -0,0 +1,25 @@
+#Link: https://dmoj.ca/problem/ccc19j2
+#Made by Vishwas Puri
+
+L = int(input())
+
+for i in range(0,L):
+    a, b = input().split()
+    print(str(b)*int(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
