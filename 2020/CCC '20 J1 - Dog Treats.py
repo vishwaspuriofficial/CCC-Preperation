76aea504f897c78acfeb52770335e6845c67b065
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-02-03 13:29:21 -0500

    .

---
+++ 2020/CCC '20 J1 - Dog Treats.py
@@ -0,0 +1,27 @@
+#Link: https://dmoj.ca/problem/ccc20j1
+#Made by Vishwas Puri
+
+S = int(input())
+M = int(input())
+L = int(input())
+
+if 1*S + 2*M + 3*L >=10:
+    print("happy")
+else:
+    print("sad")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
