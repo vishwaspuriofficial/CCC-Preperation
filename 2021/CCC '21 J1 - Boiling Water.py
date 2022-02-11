24cd402db1d789a2e588ed9cdd4ea65a016c8b76
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-02-10 19:24:55 -0500

    .

---
+++ 2021/CCC '21 J1 - Boiling Water.py
@@ -0,0 +1,32 @@
+#Link: https://dmoj.ca/problem/ccc21j1
+#Made by Vishwas Puri
+
+B = int(input())
+
+ap = 5*B-400
+
+if ap>100:
+    level = -1
+elif ap==100:
+    level = 0
+else:
+    level = 1
+
+
+print(5*B-400)
+print(level)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
